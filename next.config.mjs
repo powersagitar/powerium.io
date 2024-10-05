377e0ac81e411d32f33d0d01317f45fdaecaea81
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-05 17:25:47 -0400

    Add redirects for repo update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // temporary redirects
+  async redirects() {
+    return [
+      {
+        source:
+          '/decoding-conditionals-a-dive-into-if-else-switch-case-lookup-tables-and-interfaces',
+        destination:
+          '/blog/2024/02/01/decoding-conditionals-a-dive-into-if-else-switch-case-lookup-tables-and-interfaces',
+        permanent: true,
+      },
+      {
+        source: '/connections',
+        destination: '/contact',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
