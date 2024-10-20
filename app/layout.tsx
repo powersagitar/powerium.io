7808f3def376d0acdb1af153d5bfd6d16a80f18f
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-20 14:08:52 -0400

    Use `next-themes` for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 
 import Header from '@/components/header/header';
+import Providers from '@/components/providers';
 import { Link } from '@/components/ui/link';
 import { Toaster } from '@/components/ui/toaster';
 import { siteConfig } from '@/site.config';
@@ -31,35 +32,41 @@ export default function RootLayout({
   children: Readonly<React.ReactNode>;
 }) {
   return (
-    <html lang="en" className="flex flex-col items-center scroll-smooth">
+    <html
+      lang="en"
+      className="flex flex-col items-center scroll-smooth"
+      suppressHydrationWarning
+    >
       <body className="min-h-screen flex flex-col px-4 lg:px-0 w-full lg:w-2/3 bg-white text-black dark:bg-black dark:text-white">
-        <Header />
+        <Providers>
+          <Header />
 
-        <main className="mt-32 mb-20 scroll-mt-32 flex grow flex-col items-center">
-          {children}
-          <Analytics />
-        </main>
+          <main className="mt-32 mb-20 scroll-mt-32 flex grow flex-col items-center">
+            {children}
+            <Analytics />
+          </main>
 
-        <Toaster />
+          <Toaster />
 
-        <footer className="flex flex-col items-center py-2 text-center text-sm text-neutral-500 dark:text-neutral-400">
-          <p>
-            Copyright &copy; {new Date().getUTCFullYear()}{' '}
-            <Link href={siteConfig.metadata.author.url ?? '/'}>
-              <strong>{siteConfig.metadata.author.name}</strong>
-            </Link>
-          </p>
-          <p>
-            {/* spans for line breaking: https://stackoverflow.com/a/24357132/20143641 */}
-            <span className="inline-block">Contents are licensed under</span>{' '}
-            <span className="inline-block">
-              <Link href="https://creativecommons.org/licenses/by-sa/4.0/deed.en">
-                <strong>CC BY-SA 4.0</strong>
+          <footer className="flex flex-col items-center py-2 text-center text-sm text-neutral-500 dark:text-neutral-400">
+            <p>
+              Copyright &copy; {new Date().getUTCFullYear()}{' '}
+              <Link href={siteConfig.metadata.author.url ?? '/'}>
+                <strong>{siteConfig.metadata.author.name}</strong>
               </Link>
-            </span>{' '}
-            <span className="inline-block">unless otherwise noted.</span>
-          </p>
-        </footer>
+            </p>
+            <p>
+              {/* spans for line breaking: https://stackoverflow.com/a/24357132/20143641 */}
+              <span className="inline-block">Contents are licensed under</span>{' '}
+              <span className="inline-block">
+                <Link href="https://creativecommons.org/licenses/by-sa/4.0/deed.en">
+                  <strong>CC BY-SA 4.0</strong>
+                </Link>
+              </span>{' '}
+              <span className="inline-block">unless otherwise noted.</span>
+            </p>
+          </footer>
+        </Providers>
       </body>
     </html>
   );
