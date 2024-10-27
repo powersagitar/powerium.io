9f3a078a20db9d7614984c9b12ded70bbb8d2289
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-27 14:32:07 -0400

    Remove header padding

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -5,7 +5,7 @@ import Mobile from './mobile';
 
 export default function Header() {
   return (
-    <header className="backdrop-blur sticky flex items-center top-0 h-12 z-50 px-10 lg:px-0">
+    <header className="backdrop-blur sticky flex items-center top-0 h-12 z-50">
       <Desktop />
       <Mobile />
     </header>
