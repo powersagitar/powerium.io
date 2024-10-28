ba1156c87843eeb90ac1012f3ccd85b39a63e320
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-27 22:23:14 -0400

    Update api route

--- lib/fetch-custom-pages.ts
+++ lib/fetch-custom-pages.ts
@@ -1,5 +1,5 @@
 export default async function fetchCustomPages() {
-  const response = await fetch('/api/custom-pages');
+  const response = await fetch('/api/site-config/custom-pages');
 
   return response.json();
 }
