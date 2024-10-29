d80545f9cac64705d29ec47e2b3a91656f1499d8
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-10-28 23:43:16 -0400

    Update callout colors

--- components/ui/callout.tsx
+++ components/ui/callout.tsx
@@ -11,11 +11,11 @@ export function CalloutBadge({
   return (
     <span
       className={clsx('flex items-center', {
-        'text-blue-700 dark:text-blue-400': color === 'blue',
-        'text-green-700 dark:text-green-400': color === 'green',
-        'text-violet-700 dark:text-violet-400': color === 'violet',
-        'text-yellow-700 dark:text-yellow-400': color === 'yellow',
-        'text-red-700 dark:text-red-400': color === 'red',
+        'text-blue-600 dark:text-blue-500': color === 'blue',
+        'text-green-600 dark:text-green-500': color === 'green',
+        'text-violet-600 dark:text-violet-500': color === 'violet',
+        'text-yellow-600 dark:text-yellow-500': color === 'yellow',
+        'text-red-600 dark:text-red-500': color === 'red',
         'text-inherit': color === 'inherit',
       })}
     >
@@ -34,11 +34,11 @@ export function Callout({
   return (
     <div
       className={clsx('mt-6 border-l-2 pl-6', {
-        'border-blue-700 dark:border-blue-400': borderColor === 'blue',
-        'border-green-700 dark:border-green-400': borderColor === 'green',
-        'border-violet-700 dark:border-violet-400': borderColor === 'violet',
-        'border-yellow-700 dark:border-yellow-400': borderColor === 'yellow',
-        'border-red-700 dark:border-red-400': borderColor === 'red',
+        'border-blue-600 dark:border-blue-500': borderColor === 'blue',
+        'border-green-600 dark:border-green-500': borderColor === 'green',
+        'border-violet-600 dark:border-violet-500': borderColor === 'violet',
+        'border-yellow-600 dark:border-yellow-500': borderColor === 'yellow',
+        'border-red-600 dark:border-red-500': borderColor === 'red',
         'border-inherit': borderColor === 'inherit',
       })}
     >
