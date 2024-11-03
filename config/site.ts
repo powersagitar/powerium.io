ae10de687e82df18ed35b88060f52dd7b3b1ef20
JIHO JEON <76805323+zeonjiho@users.noreply.github.com>
2024-11-03 17:04:45 +0900

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,24 @@
 import { SiteConfig } from '@/lib/config/site';
 
 export const siteConfig: SiteConfig = {
+  // URL 관련 정보 설정
   url: {
-    protocol: 'https',
-    hostname: 'www.powerium.io',
-    origin: 'https://www.powerium.io',
+    protocol: 'https',                // 사이트의 프로토콜 설정
+    hostname: 'www.zeonjiho.io',      // 사이트의 호스트 이름
+    origin: 'https://www.zeonjiho.io', // 사이트의 전체 URL
   },
 
+  // 메타데이터 설정
   metadata: {
-    author: { name: 'powersagitar', url: '/about' },
-    title: 'powerium.io',
-    description:
-      "@powersagitar's blog about software development, productivity, and many more.",
+    author: { 
+      name: 'zeonjiho',              // 사이트 저자의 이름
+      url: '/about'                  // 저자에 대한 정보 페이지 경로
+    },
+    title: 'zeonjiho.io',            // 사이트 제목
+    description:                     // 사이트 설명
+      "@zeonjiho's blog about CGI development, productivity, and many more.",
   },
 
-  githubRepository: 'powersagitar/powerium.io',
+  // GitHub 저장소 정보
+  githubRepository: 'zeonjiho/zeonjiho_blog',  // GitHub 저장소의 경로
 };
