8211e4804027beb02ec3b49d0e375a81b906df8d
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-11-04 20:35:12 -0500

    Refactor Table of Contents component to use Sheet for better UX

--- components/table-of-contents/Desktop.tsx
+++ components/table-of-contents/Desktop.tsx
@@ -6,8 +6,16 @@ import {
   Heading3BlockObjectResponse,
 } from '@notionhq/client/build/src/api-endpoints';
 
-import { Separator } from '../ui/separator';
-import { Ul } from '../ui/typography';
+import { ScrollArea } from '../ui/scroll-area';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '../ui/sheet';
+import { Large, Ul } from '../ui/typography';
 import { NotionPageHeadingLi } from './TableOfContents';
 
 export default function Desktop({
@@ -19,51 +27,34 @@ export default function Desktop({
     | Heading3BlockObjectResponse
   )[];
 }) {
-  const [displayTocSidebar, setDisplayTocPanel] = useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
     <aside className="flex fixed top-0 right-0 bottom-0 items-center z-50">
-      {displayTocSidebar ? (
-        <div onMouseLeave={() => setDisplayTocPanel(false)}>
-          <Sidebar>{notionPageHeadings}</Sidebar>
-        </div>
-      ) : (
-        <div onMouseEnter={() => setDisplayTocPanel(true)}>
-          <Hint />
-        </div>
-      )}
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger asChild onMouseEnter={() => setOpen(true)}>
+          <span className="[writing-mode:vertical-rl] rounded backdrop-blur">
+            Table of Contents
+          </span>
+        </SheetTrigger>
+        <SheetContent side="right">
+          <SheetHeader>
+            <SheetTitle>
+              <Large>Table of Contents</Large>
+            </SheetTitle>
+            <SheetDescription className="sr-only">
+              Table of Contents of the current page
+            </SheetDescription>
+          </SheetHeader>
+          <ScrollArea>
+            <nav className="flex flex-col backdrop-blur-lg p-4 rounded-2xl max-w-prose max-h-[75vh]">
+              <Ul className="my-0" onClick={() => setOpen(false)}>
+                <NotionPageHeadingLi>{notionPageHeadings}</NotionPageHeadingLi>
+              </Ul>
+            </nav>
+          </ScrollArea>
+        </SheetContent>
+      </Sheet>
     </aside>
-  );
-}
-
-function Hint() {
-  return (
-    <div className="flex">
-      <span className="[writing-mode:vertical-rl] py-3 rounded-l-lg backdrop-blur">
-        Table of Contents
-      </span>
-    </div>
-  );
-}
-
-function Sidebar({
-  children: notionPageHeadings,
-}: {
-  children: (
-    | Heading1BlockObjectResponse
-    | Heading2BlockObjectResponse
-    | Heading3BlockObjectResponse
-  )[];
-}) {
-  return (
-    <nav className="flex flex-col backdrop-blur-lg p-4 rounded-2xl max-w-prose max-h-[75vh]">
-      <h1 className="self-center font-medium">Table of Contents</h1>
-
-      <Separator className="my-2" />
-
-      <Ul className="my-0">
-        <NotionPageHeadingLi>{notionPageHeadings}</NotionPageHeadingLi>
-      </Ul>
-    </nav>
   );
 }
