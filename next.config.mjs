30597f8d466131d155a3463059c9fc3af556ccd1
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-17 00:23:18 -0500

    set up permanent redirects for new blog identifiers

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,22 @@ const nextConfig = {
         destination: '/contact',
         permanent: true,
       },
+      {
+        source: '/blog/2025/01/16/mastermind-the-ics4u-culminating-project',
+        destination: '/blog/17d5f284f8a480988a9ad20e5fd4fa57',
+        permanent: true,
+      },
+      {
+        source: '/blog/2024/11/09/configuring-powersagitar-powerium-io',
+        destination: '/blog/13a5f284f8a4801b946bdaf8c44c59a2',
+        permanent: true,
+      },
+      {
+        source:
+          '/blog/2024/02/01/decoding-conditionals-a-dive-into-if-else-switch-case-lookup-tables-and-interfaces',
+        destination: '/blog/1165f284f8a480a8b9c7c84f9c76ba69',
+        permanent: true,
+      },
     ];
   },
   images: {
