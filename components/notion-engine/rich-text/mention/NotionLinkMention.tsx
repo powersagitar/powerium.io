459e18776508f152c95f759726ea2419ce64793a
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-18 11:20:59 -0500

    add basic link mention support

---
+++ components/notion-engine/rich-text/mention/NotionLinkMention.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+
+import { MentionRichTextItemResponse } from '@notionhq/client/build/src/api-endpoints';
+
+import { Link } from '@/components/ui/link';
+
+export type MentionLinkMentionResponse = {
+  annotations: MentionRichTextItemResponse['annotations'];
+  href: MentionRichTextItemResponse['href'];
+  plain_text: MentionRichTextItemResponse['plain_text'];
+  type: MentionRichTextItemResponse['type'];
+  mention: {
+    type: 'link_mention';
+    link_mention: {
+      description: string;
+      href: string;
+      icon_url: string;
+      link_provider: string;
+      thumbnail_url: string;
+      title: string;
+    };
+  };
+};
+
+type NotionLinkMentionProps = {
+  mention: MentionLinkMentionResponse;
+};
+
+export default function NotionLinkMention({ mention }: NotionLinkMentionProps) {
+  if (mention.mention.type !== 'link_mention') {
+    console.error('Non-link_mention mention passed to NotionLinkMention');
+    return <>Non-link_mention mention passed to NotionLinkMention</>;
+  }
+
+  const { title, href, icon_url, link_provider } = mention.mention.link_mention;
+
+  return (
+    <Link href={href} className="flex items-center no-underline">
+      <Image
+        src={icon_url}
+        alt="Notion link preview icon"
+        height={22}
+        width={22}
+        className="mr-1"
+      />
+      <span className="mr-1 text-muted-foreground">{link_provider}</span>
+      <span className="underline">{title}</span>
+    </Link>
+  );
+}
--- components/notion-engine/rich-text/mention/NotionMentionRichTextItem.tsx
+++ components/notion-engine/rich-text/mention/NotionMentionRichTextItem.tsx
@@ -1,10 +1,13 @@
 import { MentionRichTextItemResponse } from '@notionhq/client/build/src/api-endpoints';
 
 import NotionDateMention from './NotionDateMention';
+import NotionLinkMention, {
+  MentionLinkMentionResponse,
+} from './NotionLinkMention';
 import NotionPageMention from './NotionPageMention';
 
 type NotionMentionRichTextItemProps = {
-  mention: MentionRichTextItemResponse;
+  mention: MentionRichTextItemResponse | MentionLinkMentionResponse;
 };
 
 export default function NotionMentionRichTextItem({
@@ -17,11 +20,20 @@ export default function NotionMentionRichTextItem({
     case 'link_preview':
       return <>link_preview</>;
 
+    case 'link_mention':
+      return (
+        <NotionLinkMention mention={mention as MentionLinkMentionResponse} />
+      );
+
     case 'date':
-      return <NotionDateMention mention={mention} />;
+      return (
+        <NotionDateMention mention={mention as MentionRichTextItemResponse} />
+      );
 
     case 'page':
-      return <NotionPageMention mention={mention} />;
+      return (
+        <NotionPageMention mention={mention as MentionRichTextItemResponse} />
+      );
 
     case 'database':
       return <>database</>;
