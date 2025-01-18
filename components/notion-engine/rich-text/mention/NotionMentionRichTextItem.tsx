89e8067422198e4dd0603a2dcccb5efd99744f74
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-18 17:07:13 -0500

    add NotionUserMention component and refactor NotionMentionRichTextItem for improved rendering

--- components/notion-engine/rich-text/mention/NotionMentionRichTextItem.tsx
+++ components/notion-engine/rich-text/mention/NotionMentionRichTextItem.tsx
@@ -1,44 +1,48 @@
-import { MentionRichTextItemResponse } from '@notionhq/client/build/src/api-endpoints';
+import { JSX } from 'react';
+
+import { MentionRichTextItemResponse as SDKMentionRichTextItemResponse } from '@notionhq/client/build/src/api-endpoints';
 
 import NotionDateMention from './NotionDateMention';
 import NotionLinkMention, {
   MentionLinkMentionResponse,
 } from './NotionLinkMention';
 import NotionPageMention from './NotionPageMention';
+import NotionUserMention from './NotionUserMention';
+
+type MentionRichTextItemResponse =
+  | SDKMentionRichTextItemResponse
+  | MentionLinkMentionResponse;
+
+type Renderers = {
+  [key in MentionRichTextItemResponse['mention']['type']]: (
+    mention: MentionRichTextItemResponse,
+  ) => JSX.Element;
+};
+
+const renderers: Renderers = {
+  user: (mention) => (
+    <NotionUserMention mention={mention as SDKMentionRichTextItemResponse} />
+  ),
+  link_preview: () => <>link_preview</>,
+  link_mention: (mention) => (
+    <NotionLinkMention mention={mention as MentionLinkMentionResponse} />
+  ),
+  date: (mention) => (
+    <NotionDateMention mention={mention as SDKMentionRichTextItemResponse} />
+  ),
+  page: (mention) => (
+    <NotionPageMention mention={mention as SDKMentionRichTextItemResponse} />
+  ),
+  database: () => <>database</>,
+  template_mention: () => <>template_mention</>,
+};
 
 type NotionMentionRichTextItemProps = {
-  mention: MentionRichTextItemResponse | MentionLinkMentionResponse;
+  mention: MentionRichTextItemResponse;
 };
 
 export default function NotionMentionRichTextItem({
   mention,
 }: NotionMentionRichTextItemProps) {
-  switch (mention.mention.type) {
-    case 'user':
-      return <>user</>;
-
-    case 'link_preview':
-      return <>link_preview</>;
-
-    case 'link_mention':
-      return (
-        <NotionLinkMention mention={mention as MentionLinkMentionResponse} />
-      );
-
-    case 'date':
-      return (
-        <NotionDateMention mention={mention as MentionRichTextItemResponse} />
-      );
-
-    case 'page':
-      return (
-        <NotionPageMention mention={mention as MentionRichTextItemResponse} />
-      );
-
-    case 'database':
-      return <>database</>;
-
-    case 'template_mention':
-      return <>template_mention</>;
-  }
+  return renderers[mention.mention.type](mention);
 }
