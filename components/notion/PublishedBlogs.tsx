9899e4da70d44debb1dffca9c5dd356f12a4d76f
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-02-01 11:57:28 -0500

    refactor: extract `VerticalScrollAreaWithShadow`

--- components/notion/PublishedBlogs.tsx
+++ components/notion/PublishedBlogs.tsx
@@ -15,6 +15,7 @@ import { NotionArticlePageProperties } from '@/lib/notion/types';
 import LazyLoader from '../LazyLoader';
 import NotionRichTextItems from '../notion-engine/rich-text';
 import { Link } from '../ui/link';
+import { VerticalScrollAreaWithShadow } from '../ui/scroll-area-with-shadow';
 import { Separator } from '../ui/separator';
 import { Skeleton } from '../ui/skeleton';
 import { H2 } from '../ui/typography';
@@ -66,48 +67,46 @@ export default function PublishedBlogs() {
             </div>
           }
         >
-          <div className="before:from-background after:from-background relative before:absolute before:top-0 before:right-0 before:left-0 before:z-10 before:h-6 before:bg-gradient-to-b after:absolute after:right-0 after:bottom-0 after:left-0 after:z-10 after:h-6 after:bg-gradient-to-t">
-            <div className="grid max-h-[65vh] gap-4 overflow-y-scroll py-6 md:grid-cols-[8em_4fr_6fr]">
-              {publishedArticles.map((article) => {
-                const properties =
-                  article.properties as unknown as NotionArticlePageProperties;
+          <VerticalScrollAreaWithShadow className="grid max-h-[65vh] gap-4 md:grid-cols-[8em_4fr_6fr]">
+            {publishedArticles.map((article) => {
+              const properties =
+                article.properties as unknown as NotionArticlePageProperties;
 
-                const publishDate = new Date(properties.published.date.start);
+              const publishDate = new Date(properties.published.date.start);
 
-                return (
-                  <Fragment key={article.id}>
-                    <Link href={getBlogHref(article.id)} className="contents">
-                      <span className="text-muted-foreground">
-                        {dateFormat(publishDate, 'mediumDate')}
-                      </span>
+              return (
+                <Fragment key={article.id}>
+                  <Link href={getBlogHref(article.id)} className="contents">
+                    <span className="text-muted-foreground">
+                      {dateFormat(publishDate, 'mediumDate')}
+                    </span>
 
-                      <span className="underline" tabIndex={0}>
-                        {properties.title.title.length > 0 ? (
-                          <NotionRichTextItems baseKey={article.id}>
-                            {properties.title.title}
-                          </NotionRichTextItems>
-                        ) : (
-                          <>&lt;untitled&gt;</>
-                        )}
-                      </span>
+                    <span className="underline" tabIndex={0}>
+                      {properties.title.title.length > 0 ? (
+                        <NotionRichTextItems baseKey={article.id}>
+                          {properties.title.title}
+                        </NotionRichTextItems>
+                      ) : (
+                        <>&lt;untitled&gt;</>
+                      )}
+                    </span>
 
-                      <span className="text-muted-foreground">
-                        {properties.description.rich_text.length > 0 ? (
-                          <NotionRichTextItems baseKey={article.id}>
-                            {properties.description.rich_text}
-                          </NotionRichTextItems>
-                        ) : (
-                          <>&lt;no description&gt;</>
-                        )}
-                      </span>
-                    </Link>
+                    <span className="text-muted-foreground">
+                      {properties.description.rich_text.length > 0 ? (
+                        <NotionRichTextItems baseKey={article.id}>
+                          {properties.description.rich_text}
+                        </NotionRichTextItems>
+                      ) : (
+                        <>&lt;no description&gt;</>
+                      )}
+                    </span>
+                  </Link>
 
-                    <Separator className="md:hidden" />
-                  </Fragment>
-                );
-              })}
-            </div>
-          </div>
+                  <Separator className="md:hidden" />
+                </Fragment>
+              );
+            })}
+          </VerticalScrollAreaWithShadow>
         </InfiniteScroll>
       </LazyLoader>
     </>
