11f24d6e144e05daab981d7e25e05fd858714267
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-02-02 18:50:41 -0500

    refactor: change mobile theme toggle to a drawer

--- components/toolbar/theme-toggle.tsx
+++ components/toolbar/theme-toggle.tsx
@@ -1,37 +1,66 @@
 'use client';
 
 import { useTheme } from 'next-themes';
+import { useState } from 'react';
 
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 
 import { Button } from '../ui/button';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuTrigger,
-} from '../ui/dropdown-menu';
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '../ui/drawer';
 
 export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+  const [open, setOpen] = useState(false);
+  const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button variant="ghost" size="icon">
           <SunIcon className="block dark:hidden" />
           <MoonIcon className="hidden dark:block" />
           <span className="sr-only">Toggle theme</span>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-          <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </DrawerTrigger>
+      <DrawerContent className="pb-4">
+        <DrawerHeader>
+          <DrawerTitle>Theme</DrawerTitle>
+          <DrawerDescription>Let the ambience move with you.</DrawerDescription>
+        </DrawerHeader>
+        <Button
+          variant="ghost"
+          onClick={() => {
+            setTheme('system');
+            setOpen(false);
+          }}
+        >
+          System
+        </Button>
+        <Button
+          variant="ghost"
+          onClick={() => {
+            setTheme('light');
+            setOpen(false);
+          }}
+        >
+          Light
+        </Button>
+        <Button
+          variant="ghost"
+          onClick={() => {
+            setTheme('dark');
+            setOpen(false);
+          }}
+        >
+          Dark
+        </Button>
+      </DrawerContent>
+    </Drawer>
   );
 }
