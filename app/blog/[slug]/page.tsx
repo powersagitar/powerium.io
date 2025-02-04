a8d5ca40c26b2576276e2b4cf8b8e9d8af3fb037
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-02-04 12:07:20 -0500

    refactor: extract page fetching logic

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,64 +11,65 @@ import { getBlogHref } from '@/lib/notion/client';
 import { retrieveNotionPage } from '@/lib/notion/server';
 import { NotionBlogPageProperties } from '@/lib/notion/types';
 
-type GenerateMetadataProps = {
+type Props = {
   params: Promise<{ slug: string }>;
 };
 
-export async function generateMetadata({
-  params,
-}: GenerateMetadataProps): Promise<Metadata> {
+async function getPage({ params }: Props) {
+  // exception is intentinally not caught
+  // as it triggers a 500 internal server error
+  const id = (await params).slug;
+
   try {
-    const pageId = (await params).slug;
-    const page = (await retrieveNotionPage(pageId)) as PageObjectResponse;
-    const properties = page.properties as unknown as NotionBlogPageProperties;
-
-    const title = properties.title.title
-      .map((richtext) => richtext.plain_text)
-      .join('');
-    const description = properties.description.rich_text
-      .map((richtext) => richtext.plain_text)
-      .join('');
-
-    return {
-      title,
-      description,
-
-      alternates: {
-        canonical: getBlogHref(pageId),
-      },
-
-      openGraph: {
-        title,
-        description,
-        url: `${siteConfig.url.origin}/blog/${pageId}`,
-        siteName: siteConfig.metadata.title,
-        images: [
-          {
-            url: `${siteConfig.url.origin}/api/og/blog/${pageId}`,
-          },
-        ],
-        locale: 'en_US',
-        type: 'website',
-      },
-    };
+    return (await retrieveNotionPage(id)) as PageObjectResponse;
   } catch (e) {
-    console.warn(e);
-    return {};
+    console.error(e);
+    notFound();
   }
 }
 
-type BlogArticleProps = {
-  params: Promise<{ slug: string }>;
-};
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  // exception is intentinally not caught
+  // as it triggers a 500 internal server error
+  const page = await getPage(props);
+  const properties = page.properties as NotionBlogPageProperties;
 
-export default async function BlogArticle({ params }: BlogArticleProps) {
-  try {
-    const pageId = (await params).slug;
-    const page = (await retrieveNotionPage(pageId)) as PageObjectResponse;
-    return <NotionPageBlog page={page} />;
-  } catch (e) {
-    console.warn(e);
-    return notFound();
-  }
+  const title = properties.title.title
+    .map((richtext) => richtext.plain_text)
+    .join('');
+  const description = properties.description.rich_text
+    .map((richtext) => richtext.plain_text)
+    .join('');
+  const canonical = getBlogHref(page.id);
+  const url = canonical;
+  const siteName = siteConfig.metadata.title;
+  const ogImageURL = new URL('/api/og/blog/' + page.id, siteConfig.url.origin);
+
+  return {
+    title,
+    description,
+    alternates: {
+      canonical,
+    },
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName,
+      images: [
+        {
+          url: ogImageURL,
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+  };
+}
+
+export default async function BlogArticle(props: Props) {
+  // exception is intentinally not caught
+  // as it triggers a 500 internal server error
+  const page = await getPage(props);
+  return <NotionPageBlog page={page} />;
 }
