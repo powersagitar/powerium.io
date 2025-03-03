99bdbc73423948b3b2a85a92f7b201ac8a2400e2
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-02 21:13:57 -0500

    refactor: remove api route `/api/notion/block-children`

--- www/lib/notion/client.ts
+++ www/lib/notion/client.ts
@@ -1,28 +1,10 @@
 import {
-  ListBlockChildrenResponse,
   PageObjectResponse,
   QueryDatabaseResponse,
 } from '@notionhq/client/build/src/api-endpoints';
 
 export function getBlogHref(pageId: PageObjectResponse['id']) {
   return `/blog/${pageId}`;
-}
-
-export async function retrieveNotionBlockChildren(
-  blockId: string,
-  startCursor?: string | null,
-): Promise<ListBlockChildrenResponse> {
-  const searchParams = new URLSearchParams({
-    'block-id': blockId,
-  });
-
-  if (startCursor) {
-    searchParams.set('start-cursor', startCursor);
-  }
-
-  return fetch(`/api/notion/block-children?${searchParams.toString()}`, {
-    next: { revalidate: 14400 },
-  }).then((response) => response.json());
 }
 
 export async function retrievePublishedArticles(
