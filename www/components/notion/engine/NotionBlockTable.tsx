2209d205af98a29e6393ff14cfa82adb28c8efbe
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-02 21:09:31 -0500

    refactor: load notion table data on server

--- www/components/notion/engine/NotionBlockTable.tsx
+++ www/components/notion/engine/NotionBlockTable.tsx
@@ -1,77 +1,61 @@
-'use client';
-
-import { useState } from 'react';
+import { Suspense } from 'react';
 
 import {
   TableBlockObjectResponse,
   TableRowBlockObjectResponse,
 } from '@notionhq/client/build/src/api-endpoints';
 
-import { retrieveNotionBlockChildren } from '@/lib/notion/client';
+import { retrieveNotionBlockChildrenAll } from '@/lib/notion/server';
 
 import { Table, Td, Th, Tr } from '../../ui/typography';
-import LazyLoader from '../../utils/lazy-loader';
 import NotionRichTextItems from './rich-text';
 
-// TODO: fetch data on server and use suspense
-
-export default function NotionBlockTable({
-  table,
-}: {
+type NotionBlockTableProps = {
   table: TableBlockObjectResponse;
-}) {
-  const [rows, setRows] = useState<TableRowBlockObjectResponse[]>([]);
-  const [startCursor, setStartCursor] = useState<string | null | undefined>(
-    undefined,
-  );
-  const [lazyLoaderId, setLazyLoaderId] = useState(0);
+};
+
+async function NotionBlockTableSuspended({ table }: NotionBlockTableProps) {
+  const rows = (await retrieveNotionBlockChildrenAll(
+    table.id,
+  )) as TableRowBlockObjectResponse[];
 
   return (
-    <LazyLoader
-      load={() => {
-        retrieveNotionBlockChildren(table.id, startCursor).then((response) => {
-          setStartCursor(response.next_cursor);
-          setRows([
-            ...rows,
-            ...(response.results as TableRowBlockObjectResponse[]),
-          ]);
+    <Table>
+      <tbody>
+        {rows.map((row, rowIdx) => (
+          <Tr key={row.id}>
+            {row.table_row.cells.map((richTextCell, cellIdx) => {
+              const key = `${row.id}-${cellIdx}`;
 
-          if (response.next_cursor !== null) {
-            setLazyLoaderId(lazyLoaderId + 1);
-          }
-        });
-      }}
-      id={lazyLoaderId}
-    >
-      <Table>
-        <tbody>
-          {rows.map((row, rowIdx) => (
-            <Tr key={row.id}>
-              {row.table_row.cells.map((richTextCell, cellIdx) => {
-                const key = `${row.id}-${cellIdx}`;
+              const cell = (
+                <NotionRichTextItems
+                  baseKey={`${row.id}-${cellIdx}`}
+                  richText={richTextCell}
+                />
+              );
 
-                const cell = (
-                  <NotionRichTextItems
-                    baseKey={`${row.id}-${cellIdx}`}
-                    richText={richTextCell}
-                  />
-                );
+              const isColHeader = table.table.has_column_header && rowIdx === 0;
 
-                const isColHeader =
-                  table.table.has_column_header && rowIdx === 0;
+              const isRowHeader = table.table.has_row_header && cellIdx === 0;
 
-                const isRowHeader = table.table.has_row_header && cellIdx === 0;
+              if (isColHeader || isRowHeader) {
+                return <Th key={key}>{cell}</Th>;
+              }
 
-                if (isColHeader || isRowHeader) {
-                  return <Th key={key}>{cell}</Th>;
-                }
+              return <Td key={key}>{cell}</Td>;
+            })}
+          </Tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
 
-                return <Td key={key}>{cell}</Td>;
-              })}
-            </Tr>
-          ))}
-        </tbody>
-      </Table>
-    </LazyLoader>
+export default function NotionBlockTable({ table }: NotionBlockTableProps) {
+  // TODO: fallback to be implemented
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <NotionBlockTableSuspended table={table} />
+    </Suspense>
   );
 }
