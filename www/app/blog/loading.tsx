facd4ea9c9a5a7c619606878d5b3c7de65db404b
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-13 21:20:44 -0400

    feat: adapt skeleton for mobile `/blog`

--- www/app/blog/loading.tsx
+++ www/app/blog/loading.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 
+import { Separator } from '@/components/ui/separator';
 import { Skeleton } from '@/components/ui/skeleton';
 import { H1 } from '@/components/ui/typography';
 
@@ -11,9 +12,11 @@ export default function PublishedBlogLoading() {
       <div className="my-4 grid gap-4 md:grid-cols-[8em_4fr_6fr]">
         {Array.from({ length: 10 }).map((_, index) => (
           <Fragment key={`blog-entry-skeleton-${index}`}>
+            <Skeleton className="h-6 w-1/3 md:w-full" />
             <Skeleton className="h-6" />
             <Skeleton className="h-6" />
-            <Skeleton className="h-6" />
+
+            <Separator className="md:hidden" />
           </Fragment>
         ))}
       </div>
