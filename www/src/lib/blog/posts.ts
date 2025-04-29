d13ad780dd1912b1eaf99b6051f994324ef0a637
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-29 17:37:29 -0400

    feat: finalize `getAllPosts()` prototype

--- www/src/lib/blog/posts.ts
+++ www/src/lib/blog/posts.ts
@@ -1,5 +1,18 @@
+import { readFileSync } from "fs";
 import { globbySync } from "globby";
+import matter from "gray-matter";
+import { Blog, Metadata } from "./types";
 
-export function getAllPosts() {
-  return globbySync("content/blog/**/*.mdx");
+export function getAllPosts(): Blog[] {
+  const paths = globbySync("content/blog/**/*.mdx");
+
+  return paths.map((path) => {
+    const post = readFileSync(path);
+    const metadata = matter(post).data as Metadata;
+
+    return {
+      path,
+      metadata,
+    };
+  });
 }
