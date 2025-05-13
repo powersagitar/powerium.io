6e177e5354f99a2ea8e4705ace555d11fe1623ad
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-05-12 22:10:19 -0400

    chore: add one more line to copyright notice

--- www/src/components/layout/footer/copyright.tsx
+++ www/src/components/layout/footer/copyright.tsx
@@ -3,8 +3,12 @@ import { siteConfig } from "@/lib/config/config";
 
 export default function Copyright() {
   return (
-    <Muted className="mx-4">
-      Copyright &copy; 2025 {siteConfig.author}. Some rights reserved.
-    </Muted>
+    <div className="mx-4 flex flex-col gap-y-1">
+      <Muted>
+        Copyright &copy; {new Date().getUTCFullYear()} {siteConfig.author}. Some
+        rights reserved.
+      </Muted>
+      <Muted>Contents distributed under CC BY-SA 4.0.</Muted>
+    </div>
   );
 }
