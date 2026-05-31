cd2eabc8b0c5bfc98f71b988ccdaed92f33c0eb5
powersagitar <114868133+powersagitar@users.noreply.github.com>
2026-05-30 21:00:29 -0400

    feat: add RssConfig type to SiteConfig and install feed

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,3 +1,7 @@
+export type RssConfig = {
+  include: 'none' | 'all' | string[];
+};
+
 export type SiteConfig = {
   name: string;
   description: string;
@@ -5,4 +9,5 @@ export type SiteConfig = {
   author?: string;
   repository?: string;
   branch?: string;
+  rss?: RssConfig;
 };
